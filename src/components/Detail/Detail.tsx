890987606ab69ae94c89854fb00010321eb86a88
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-25 16:23:37 -0600

    feat: Added material icons from google

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -1,10 +1,21 @@
 import './Detail.css';
+// import { Sunny } from '@material-ui/icons'
 
 const Detail = () => {
     return (
-        <>
-            <p>Details here</p>
-        </>
+        <div className='detail-page'>
+            <img></img>
+            <div className='at-a-glance'>
+                <h1>Common Name</h1>
+                <h2>Scientific Name</h2>
+                <p>type</p>
+                <p>color</p>
+                <p><span className="material-symbols-rounded">sunny</span>sun requirement</p>
+                <p><span className="material-symbols-rounded">calendar_month</span>perennial or annual</p>
+                <p><span className="material-symbols-rounded">water_drop</span>watering frequency</p>
+                <p><span className="material-symbols-rounded">location_on</span>hardiness zones</p>
+            </div>
+        </div>
     )
 }
 
