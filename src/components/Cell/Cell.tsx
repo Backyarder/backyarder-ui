b43f44f84bd5e995557cb9e953442ed69acb9395
Matt Fowler <mbfowler@gmail.com>
2023-08-26 14:55:43 -0400

    Add grid and cells with initial styling

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,6 +1,23 @@
-const Cell = () => {
+import { useState } from 'react';
+import './Cell.scss'
+
+type GridCell = {
+    id: string;
+}
+
+const Cell = ({id}: GridCell) => {
+    const [isDisabled, setIsDisabled] = useState(false)
+
+    const handleClick = (e: React.MouseEvent) => {
+        setIsDisabled(!isDisabled)
+        const target = e.target as Element
+        !isDisabled ? target.classList.add('disabled') : target.classList.remove('disabled')
+    }
+
     return (
-        <></>
+        <div id={id} className='cell' onClick={handleClick}>
+            {id}
+        </div>
     )
 }
 
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,6 +1,14 @@
+import Cell from '../Cell/Cell'
+import './Grid.scss'
+import { cellsMockData } from './cellsMockData'
+
 const Grid = () => {
+    const cells = cellsMockData.map(cell => <Cell id={cell.id} key={cell.id}/>)
+
     return (
-        <></>
+        <section id='grid'>
+            {cells}
+        </section>
     )
 }
 
