843612c98c5b622930ab7e9e7bb4fc705ece5003
Daniel Cochico <dcochico.20@gmail.com>
2023-08-26 19:27:11 -0700

    Implement conditional rendering to Main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,19 @@
 // import './Main.css';
-import Sidebar from '../Sidebar/Sidebar'
-import Grid from '../Grid/Grid'
-import Nav from '../Nav/Nav'
-import List from '../List/List'
-import { Link } from 'react-router-dom'
+import Sidebar from '../Sidebar/Sidebar';
+import Grid from '../Grid/Grid';
+import Nav from '../Nav/Nav';
+import List from '../List/List';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Main = () => {
+  const [isGardenView, setIsGardenView] = useState<boolean>(true);
+
   return (
     <>
       <Sidebar />
-      <Grid />
-      <List />
-      <Nav />
+      {isGardenView ? <Grid /> : <List />}
+      <Nav isGardenView={isGardenView} setIsGardenView={setIsGardenView} />
       <Link to='/plants'>See Plant Details Page</Link>
     </>
   );
