090965268c66e040fadf96a2497990821a546ad4
Daniel Cochico <dcochico.20@gmail.com>
2023-08-27 16:43:58 -0700

    Add garden list mock data

---
+++ src/components/List/listMockData.tsx
@@ -0,0 +1,34 @@
+export const listMockData = [
+  {
+    'plant_name': 'Apple',
+    status: 1,
+    'plant_id': 7
+  },
+  {
+    'plant_name': 'Banana',
+    status: 1,
+    'plant_id': 8
+  },
+  {
+    'plant_name': 'Banana',
+    status: 1,
+    'plant_id': 8
+  },
+  {
+    'plant_name': 'Orange',
+    status: 1,
+    'plant_id': 9
+  },
+  {
+    'plant_name': 'Orange',
+    status: 1,
+    'plant_id': 9
+  },
+  {
+    'plant_name': 'Orange',
+    status: 1,
+    'plant_id': 9
+  }
+]
+
+// Assuming status 0 === unplanted && status 1 === planted
