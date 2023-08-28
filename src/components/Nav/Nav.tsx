727d5938c105b6abbe4a8a44853728112be28ddb
Daniel Cochico <dcochico.20@gmail.com>
2023-08-28 02:44:24 -0700

    Add conditional rendering for pop up prompt

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,6 @@
 // import Button from '../Button/Button';
 // import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import './Nav.scss';
 
 type NavProps = {
@@ -8,8 +9,24 @@ type NavProps = {
 }
 
 const Nav = ({ isGardenView, setIsGardenView }: NavProps) => {
+  const [popUp, setPopUp] = useState<boolean>(false);
+
   const toggleView = () : void => {
     setIsGardenView(!isGardenView);
+  }
+
+  const handlePopUp = () : void => {
+    setPopUp(true);
+  }
+
+  const handleDelete = () : void => {
+    // Delete logic goes here.
+    setPopUp(false);
+    alert('Garden cleared.')
+  }
+
+  const handleCancel = () : void => {
+    setPopUp(false);
   }
 
   return (
@@ -36,7 +53,20 @@ const Nav = ({ isGardenView, setIsGardenView }: NavProps) => {
       >
         GARDEN VIEW
       </button>
-      {isGardenView && <button className='clear-button' >CLEAR GARDEN</button>}
+      {isGardenView &&
+        <>
+          <button className='clear-button' onClick={handlePopUp} >CLEAR GARDEN</button>
+          {popUp &&
+            <div className='pop-up'>
+              <p>Are you sure you want to clear your garden?</p>
+              <div className='pop-up-button-container'>
+                <button className='pop-up-button' onClick={handleDelete} >YES</button>
+                <button className='pop-up-button' onClick={handleCancel} >NO</button>
+              </div>
+            </div>
+          }
+        </>
+      }
     </nav>
   );
 }
