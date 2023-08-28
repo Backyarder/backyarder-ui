d01872591c5f6c1e796fa6cab910a6d21c892604
Daniel Cochico <dcochico.20@gmail.com>
2023-08-28 08:00:30 -0700

    Add dev dependencies and alt tag to image

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -30,7 +30,7 @@ const List = () => {
     return (
       <div key={plant['plant_id']} className='plant-element' >
         <div className='plant-info-container' >
-          <img src={plant.image} className='plant-image' />
+          <img src={plant.image} className='plant-image' alt={plant['plant_name']} />
           <p>{plant['plant_name'].toUpperCase()} x{findQuantity(listMockData, plant['plant_id'])}</p>
         </div>
         <NavLink to={`/plants/${plant['plant_id']}`} >
