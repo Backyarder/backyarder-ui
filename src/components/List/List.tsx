2d5ca1a4f062399924ae318e7440dcc464acdf80
Daniel Cochico <dcochico.20@gmail.com>
2023-08-27 23:20:50 -0700

    Convert List component to TypeScript

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,7 +1,43 @@
+import { listMockData } from './listMockData';
+import './List.scss';
+
+type Plant = {
+  'plant_name': string,
+  'plant_id': number
+};
+
 const List = () => {
+  const findQuantity = (array: Plant[], id: number): number => {
+    let count = 0;
+    for (const element of array) {
+      if (element['plant_id'] === id) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  const uniquePlants = listMockData.reduce((array: Plant[], plant: Plant): Plant[] => {
+    if (!array.some(item => item['plant_id'] === plant['plant_id'])) {
+      array.push(plant);
+    }
+    return array;
+  }, []);
+
+  const plantElements: JSX.Element[] = uniquePlants.map(plant => {
     return (
-        <>LIST HERE</>
-    )
+      <div className='plant-element' >
+        <p>{plant['plant_name']} x{findQuantity(listMockData, plant['plant_id'])}</p>
+        <button>VIEW PLANT DETAILS</button>
+      </div>
+    );
+  });
+
+  return (
+    <section>
+      {plantElements}
+    </section>
+  );
 }
 
-export default List
+export default List;
