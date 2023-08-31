be54d96b7bf033ecdfbb5d08a3f063726115d5da
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-31 14:11:20 -0600

    feat: Build search bar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,26 @@
 import Card from "../Card/Card"
 import './Sidebar.scss'
 import { plantsMockData } from "./plantsMockData"
+import { useState, ChangeEvent } from 'react'
 
 const Sidebar = () => {
     const cards = plantsMockData.map(plant => <Card plant={plant} key={plant.id} />)
 
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+      };
+
     return (
         <section id='plants'>
             <h2>PLANTS</h2>
-            <p>SEARCH</p>
+            <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
             {cards}
         </section>
     )
