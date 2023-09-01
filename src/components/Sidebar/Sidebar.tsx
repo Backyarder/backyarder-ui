b3b868c3775cbf70a30d1fa99f0c2ad35e03b16d
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-09-01 17:05:51 -0600

    feat: Psuedocoded search functionality with fetch

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,6 +8,13 @@ const Sidebar = () => {
 
     const [searchTerm, setSearchTerm] = useState('');
 
+    //when the user adds a search term the state will be updated
+    // when the user clicks the search button a onClick function will be fired off
+        //onClick will display a loading message in the sidebar, run a fetch call to get the search cards
+            //if response is good a cards will be updated and displayed - need to figure this one out a little more
+            // if the response is bad then the user will get a message saying that there are no plants mathcing their search OR there was an error on the server side and to try agian later
+            
+
     const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(event.target.value);
     };
