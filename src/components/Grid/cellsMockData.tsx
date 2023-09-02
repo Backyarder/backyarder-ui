3905bfa5dd14e090b6e9c9f25007c6a565fc2b37
Daniel Cochico <dcochico.20@gmail.com>
2023-09-01 19:52:11 -0700

    Update Grid mock data

--- src/components/Grid/cellsMockData.tsx
+++ src/components/Grid/cellsMockData.tsx
@@ -1,302 +1,702 @@
 export const cellsMockData = [
     {
-        id: 'a0'
+        id: 'a0',
+        status: 0,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/0/07/Honeycrisp-Apple.jpg',
+        name: 'Apple',
+        'plant_id': 7
     },
     {
-        id: 'a1'
+        id: 'a1',
+        status: 0,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg',
+        name: 'Banana',
+        'plant_id': 8
     },
     {
-        id: 'a2'
+        id: 'a2',
+        status: 0,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Orange-Fruit-Pieces.jpg',
+        name: 'Orange',
+        'plant_id': 9
     },
     {
-        id: 'a3'
+        id: 'a3',
+        status: 1,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'a4'
+        id: 'a4',
+        status: 1,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'a5'
+        id: 'a5',
+        status: 1,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'a6'
+        id: 'a6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'a7'
+        id: 'a7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'a8'
+        id: 'a8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'a9'
+        id: 'a9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b0'
+        id: 'b0',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b1'
+        id: 'b1',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b2'
+        id: 'b2',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b3'
+        id: 'b3',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b4'
+        id: 'b4',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b5'
+        id: 'b5',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b6'
+        id: 'b6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b7'
+        id: 'b7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b8'
+        id: 'b8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'b9'
+        id: 'b9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c0'
+        id: 'c0',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c1'
+        id: 'c1',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c2'
+        id: 'c2',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c3'
+        id: 'c3',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c4'
+        id: 'c4',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c5'
+        id: 'c5',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c6'
+        id: 'c6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c7'
+        id: 'c7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c8'
+        id: 'c8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'c9'
+        id: 'c9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd0'
+        id: 'd0',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd1'
+        id: 'd1',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd2'
+        id: 'd2',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd3'
+        id: 'd3',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd4'
+        id: 'd4',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd5'
+        id: 'd5',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd6'
+        id: 'd6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd7'
+        id: 'd7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd8'
+        id: 'd8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'd9'
+        id: 'd9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e0'
+        id: 'e0',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e1'
+        id: 'e1',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e2'
+        id: 'e2',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e3'
+        id: 'e3',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e4'
+        id: 'e4',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e5'
+        id: 'e5',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e6'
+        id: 'e6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e7'
+        id: 'e7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e8'
+        id: 'e8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'e9'
+        id: 'e9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f0'
+        id: 'f0',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f1'
+        id: 'f1',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f2'
+        id: 'f2',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f3'
+        id: 'f3',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f4'
+        id: 'f4',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f5'
+        id: 'f5',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f6'
+        id: 'f6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f7'
+        id: 'f7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f8'
+        id: 'f8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'f9'
+        id: 'f9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g0'
+        id: 'g0',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g1'
+        id: 'g1',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g2'
+        id: 'g2',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g3'
+        id: 'g3',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g4'
+        id: 'g4',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g5'
+        id: 'g5',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g6'
+        id: 'g6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g7'
+        id: 'g7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g8'
+        id: 'g8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'g9'
+        id: 'g9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h0'
+        id: 'h0',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h1'
+        id: 'h1',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h2'
+        id: 'h2',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h3'
+        id: 'h3',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h4'
+        id: 'h4',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h5'
+        id: 'h5',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h6'
+        id: 'h6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h7'
+        id: 'h7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h8'
+        id: 'h8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'h9'
+        id: 'h9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i0'
+        id: 'i0',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i1'
+        id: 'i1',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i2'
+        id: 'i2',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i3'
+        id: 'i3',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i4'
+        id: 'i4',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i5'
+        id: 'i5',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i6'
+        id: 'i6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i7'
+        id: 'i7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i8'
+        id: 'i8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'i9'
+        id: 'i9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j0'
+        id: 'j0',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j1'
+        id: 'j1',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j2'
+        id: 'j2',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j3'
+        id: 'j3',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j4'
+        id: 'j4',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j5'
+        id: 'j5',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j6'
+        id: 'j6',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j7'
+        id: 'j7',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j8'
+        id: 'j8',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     },
     {
-        id: 'j9'
+        id: 'j9',
+        status: 0,
+        image: null,
+        name: null,
+        'plant_id': null
     }
 ]
