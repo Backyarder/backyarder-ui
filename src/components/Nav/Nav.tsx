0a648ec527c4aac9589b669f980c3aa35da217d0
Matt Fowler <mbfowler@gmail.com>
2023-09-02 15:54:37 -0400

    Add more watering can styling

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -95,8 +95,8 @@ const Nav = ({ isGardenView, setIsGardenView, setBullDoze, setFilterGarden }: Na
               <p>UNAVAILABLE</p>
             </div>
           </div>
-          <div id='wateringCan' onClick={handleWaterAll}>
-            <img src={`${process.env.PUBLIC_URL}/images/watering-can.png`} onClick={handleWaterAll} alt='watering can, click to water all planted items'/>
+          <div id='wateringCan'>
+            <img src={`${process.env.PUBLIC_URL}/images/watering-can.png`} className='ripple' onClick={handleWaterAll} alt='watering can, click to water all planted items'/>
           </div>
           <button className='clear-button' onClick={handleFullClear} ><span className="material-symbols-rounded nav-icon">
             bomb
