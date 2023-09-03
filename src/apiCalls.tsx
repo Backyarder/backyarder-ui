b0c462d31c8e962283046cf8343fbdb03174ff3a
Daniel Cochico <dcochico.20@gmail.com>
2023-09-02 17:20:57 -0700

    Write logic for remove items network request

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -59,6 +59,18 @@ const deleteGarden = () => {
     })
 }
 
-const deleteUnplantedItems = () => {}
+const deleteUnplantedItems = () => {
+  return fetch('https://backyarder-be-47454958a7d2.herokuapp.com/api/v1/garden', {
+    method: 'DELETE'
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Unable to clear garden');
+      }
+    })
+    .catch(err => {
+      handleError(err)
+    })
+}
 
 export { getPlantList, searchPlants, getPlantDetails, patchCellContents, deleteGarden, deleteUnplantedItems }
