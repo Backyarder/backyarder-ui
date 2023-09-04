d739cb7ab4601041e79d6047e53ecd590278bf10
Matt Fowler <mbfowler@gmail.com>
2023-09-04 14:00:06 -0400

    Only allow app interaction at 1048px or more

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -19,8 +19,18 @@ const Main = () => {
   const [isGardenView, setIsGardenView] = useState<boolean>(true);
   const [bullDoze, setBullDoze] = useState<boolean>(false);
   const [filterGarden, setFilterGarden] = useState<boolean>(false);
+  const [isDesktop, setIsDesktop] = useState<boolean>(true)
   // eslint-disable-next-line
   const [apiError, setApiError] = useState<string>('')
+
+  const updateMedia = () => {
+    setIsDesktop(window.innerWidth > 1048);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
 
   useEffect(() => {
     getGarden()
@@ -39,9 +49,14 @@ const Main = () => {
 
   return (
     <main>
-      <Sidebar />
-      {isGardenView ? <Grid garden={garden} setGarden={setGarden} bullDoze={bullDoze} setBullDoze={setBullDoze} filterGarden={filterGarden} setFilterGarden={setFilterGarden} /> : <List garden={garden} />}
-      <Nav isGardenView={isGardenView} setIsGardenView={setIsGardenView} setBullDoze={setBullDoze} setFilterGarden={setFilterGarden} />
+      {isDesktop ?
+        <>
+          <Sidebar />
+          {isGardenView ? <Grid garden={garden} setGarden={setGarden} bullDoze={bullDoze} setBullDoze={setBullDoze} filterGarden={filterGarden} setFilterGarden={setFilterGarden} /> : <List garden={garden} />}
+          <Nav isGardenView={isGardenView} setIsGardenView={setIsGardenView} setBullDoze={setBullDoze} setFilterGarden={setFilterGarden} />
+        </>
+        : 'Please switch to a larger device to use this app'
+      }
     </main>
   );
 }
