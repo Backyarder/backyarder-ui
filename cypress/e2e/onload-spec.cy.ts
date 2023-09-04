c569d2f4136d61fb44df16f468054fc314b2838b
Matt Fowler <mbfowler@gmail.com>
2023-09-03 22:16:29 -0400

    Add test for drag and drop
    '
    '

--- cypress/e2e/onload-spec.cy.ts
+++ cypress/e2e/onload-spec.cy.ts
@@ -14,6 +14,10 @@ describe('Onload', () => {
   it('should display all required content', () => {
     cy.wait(['@onload-plants', '@onload-grid']).then(() => {
       cy.get('.header-logo').should('exist')
+      .get('#plants > :nth-child(3)').children().should('have.length', '30')
+      .get('#grid').children().should('have.length', 100)
+      .get('#A1').should('have.css', 'background-image', 'url("https://perenual.com/storage/species_image/2692_echinacea_tomato_soup/thumbnail/red-orange-echinacea-echinacea-cone-flower-medicinal.jpg")')
+      .get('nav').children().should('have.length', 5)
     })
   })
 })
