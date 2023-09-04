f9cda05973b45c81e6d083a46f336a382de692bf
Daniel Cochico <dcochico.20@gmail.com>
2023-09-04 01:03:13 -0700

    Write logic for list component testing suite

---
+++ cypress/e2e/list-spec.cy.ts
@@ -0,0 +1,40 @@
+describe('List Actions', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://backyarder-be-47454958a7d2.herokuapp.com/api/v1/plants', {
+      statusCode: 200,
+      fixture: 'onload-plants.json'
+    }).as('onload-plants')
+    
+    cy.intercept('GET', 'https://backyarder-be-47454958a7d2.herokuapp.com/api/v1/garden', {
+      statusCode: 200,
+      fixture: 'onload-grid.json'
+    }).as('onload-grid')
+    
+    cy.intercept('PATCH', 'https://backyarder-be-47454958a7d2.herokuapp.com/api/v1/cell', {
+      statusCode: 200,
+      fixture: 'plant-cell-response.json'
+    }).as('plant-cell')
+    
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should update quantities of new and existing plants', () => {
+    cy.wait(['@onload-plants', '@onload-grid']).then(() => {
+      cy.wait(['@onload-plants', '@onload-grid']).then(() => {
+        cy.get('#A2').click()
+        .get('.cell-button.plant-button').click()
+        .wait('@plant-cell').then(() => {
+          cy.get('.nav-button').first().click()
+          .get('.plant-info-container').contains('p', 'CONEFLOWER x2')
+          .get('.nav-button').last().click()
+          .get('#D2').click()
+          .get('.cell-button.plant-button').click()
+          .wait('@plant-cell').then(() => {
+            cy.get('.nav-button').first().click()
+            .get('.plant-info-container').last().contains('p', 'SMALL GLOBE THISTLE x1')
+          });
+        });
+      });
+    });
+  });
+})
