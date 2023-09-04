5bcae370191c32e4cc9ef1b9703409c315cc502a
Daniel Cochico <dcochico.20@gmail.com>
2023-09-03 23:55:00 -0700

    Write logic for removing unplanted items

--- cypress/e2e/nav-actions-remove-unplanted-spec.cy.ts
+++ cypress/e2e/nav-actions-remove-unplanted-spec.cy.ts
@@ -1,5 +1,30 @@
 describe('Nav Actions: Remove Unplanted Items', () => {
-  beforeEach(() => {});
+  beforeEach(() => {
+    cy.intercept('GET', 'https://backyarder-be-47454958a7d2.herokuapp.com/api/v1/plants', {
+        statusCode: 200,
+        fixture: 'onload-plants.json'
+      }).as('onload-plants');
+      
+      cy.intercept('GET', 'https://backyarder-be-47454958a7d2.herokuapp.com/api/v1/garden', {
+        statusCode: 200,
+        fixture: 'secondary-grid.json'
+      }).as('onload-grid');
+      
+      cy.intercept('DELETE', 'https://backyarder-be-47454958a7d2.herokuapp.com/api/v1/garden', {
+        statusCode: 200,
+      }).as('remove-unplanted');
+      
+      cy.visit('http://localhost:3000/');
+  });
 
-  it.skip('', () => {});
+  it('Should be able to remove unplanted items', () => {
+    cy.wait(['@onload-plants', '@onload-grid']).then(() => {
+      cy.get('.clear-button').last().click()
+      .get('.pop-up-button').first().click()
+      .wait('@remove-unplanted').then(() => {
+        cy.get('#grid').children().first().should('have.css', 'background-image', 'url("https://perenual.com/storage/species_image/2692_echinacea_tomato_soup/thumbnail/red-orange-echinacea-echinacea-cone-flower-medicinal.jpg")')
+        .get('#grid').children().last().should('have.css', 'background-color', 'rgb(120, 105, 97)');
+      });
+    });
+  });
 });
