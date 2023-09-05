35904c6a8b45871e8e9274df4d61f4348e6c47f0
Daniel Cochico <dcochico.20@gmail.com>
2023-09-05 08:15:29 -0700

    Add changes to modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -12,7 +12,7 @@ const Modal = ({ alert, bullDoze, filterGarden, toggleModal }: ModalProps) => {
     <div className='modal-overlay' >
       <div className='modal' >
         {!alert && <p>Sorry! Cell is unavailable for planting.</p>}
-        {alert && <p>Garden cleared.</p>}
+        {alert && <p>Items cleared.</p>}
         <span className="material-symbols-rounded modal-icon">potted_plant</span>
         <button className='modal-button' onClick={toggleModal} >Close</button>
       </div>
