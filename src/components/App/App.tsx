f2d2935660836cdaad937bf18b7779afcded3190
Matt Fowler <mbfowler@gmail.com>
2023-10-24 14:27:18 -0400

    Add intial email & password ui and logic, redirect on success

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import Header from '../Header/Header'
+import Login from '../Login/Login'
 import Main from '../Main/Main'
 import Detail from '../Detail/Detail'
 import Error from '../Error/Error'
@@ -10,9 +11,10 @@ const App = () => {
     <div className='app'>
       <Header />
       <Routes>
-        <Route path='/' element={<Main />} ></ Route >
-        <Route path='/plants/:id' element={<Detail />} ></ Route >
-        <Route path='*' element={<Error />} ></ Route >
+        <Route path='/' element={<Main />}></ Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/plants/:id' element={<Detail />}></ Route>
+        <Route path='*' element={<Error />}></ Route>
       </Routes>
     </div>
   );
